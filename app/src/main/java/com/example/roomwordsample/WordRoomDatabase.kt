7dc348a39f9d0b1a8bfdd18bc8a60d942fd9b0dc
John Njuki <njukijohn2000@gmail.com>
2022-06-27 21:58:23 +0300

    Create the ViewModel & Repository

---
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.roomwordsample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = [Word::class], version = 1, exportSchema = false)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
