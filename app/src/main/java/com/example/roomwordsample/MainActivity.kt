ce3f4403bb2d7c3608b6c121cee0dceba983cae7
John Njuki <njukijohn2000@gmail.com>
2022-07-25 20:24:42 +0300

    Share added word

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.roomwordsample
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomwordsample.NewWordNotification.createNotificationChannel
@@ -20,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         WordViewModelFactory((application as WordsApplication).repository)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private val getWord =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -27,16 +30,16 @@ class MainActivity : AppCompatActivity() {
                 data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
                     val word = Word(it)
                     wordViewModel.insert(word)
+
+                    notify(this, "New Word", it, 0)
                 }
-
-                notify(this, "Updated", "New word added", 0)
-
             } else {
                 Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG)
                     .show()
             }
         }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/roomwordsample/NewWordNotification.kt
+++ app/src/main/java/com/example/roomwordsample/NewWordNotification.kt
@@ -2,8 +2,11 @@ package com.example.roomwordsample
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -11,21 +14,33 @@ object NewWordNotification {
 
     private const val CHANNEL_ID = "0"
 
+    // TODO: Move this function to MainActivity
     fun createNotificationChannel(context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = R.string.channel_name.toString()
-            val descriptionText = R.string.channel_description.toString()
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
+            val channel = NotificationChannel(CHANNEL_ID, "New Word", importance).apply {
+                description = "New word added"
             }
             // Register the channel with the system
-            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun notify(context: Context, contentTitle: String, contentText: String, notificationId: Int) {
+
+        val shareIntent: PendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            Intent.createChooser(
+                Intent(Intent.ACTION_SEND).setType("text/plain")
+                    .putExtra(Intent.EXTRA_TEXT, contentText), "New word"
+            ),
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setContentTitle(contentTitle)
@@ -33,7 +48,16 @@ object NewWordNotification {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setTicker(contentTitle)
             // TODO set the pending intent
+            .setContentIntent(
+                PendingIntent.getActivity(
+                    context,
+                    0,
+                    Intent(context, MainActivity::class.java),
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            )
             .setAutoCancel(true)
+            .addAction(R.drawable.ic_baseline_share_24, "Share", shareIntent)
 
         with(NotificationManagerCompat.from(context)) {
             notify(notificationId, notificationBuilder.build())
